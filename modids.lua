ea7015049ffda2406504bd9d3dcc989854eb08c7
Crvstxl <92829005+Crvstxl@users.noreply.github.com>
2021-12-12 19:04:21 +0100

    Update modids.lua

--- modids.lua
+++ modids.lua
@@ -1,4 +1,4 @@
---// this is open sourced lol we don't give af if you leak
+-- this is open sourced lol we don't give af if you leak
 local MoonWareIds = { -- Mod Perms
     265839211, -- dawn
     1112423310, -- brennen
